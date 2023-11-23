86c2a210965695cb40521f66e3bffe0a955e09e4
seonb2n <626ksb@gmail.com>
2023-11-23 19:33:28 +0900

    - Property 설정

--- src/main/java/com/example/inflearn_spring_core_demo/book/AppRunner.java
+++ src/main/java/com/example/inflearn_spring_core_demo/book/AppRunner.java
@@ -2,6 +2,7 @@ package com.example.inflearn_spring_core_demo.book;
 
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -14,13 +15,15 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     ApplicationContext ctx;
 
-    @Autowired
-    BookRepository bookRepository;
+    @Value("${app.name")
+    String appName;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name"));
+        System.out.println(environment.getProperty("app.about"));
+
+        System.out.println(appName);
     }
 }
