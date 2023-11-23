782c2289cab0c629409b6a296e3ac4c58514004b
seonb2n <626ksb@gmail.com>
2023-11-23 19:30:52 +0900

    - EnvironmentCapable 을 상속받았기에 ApplicationContext 에서는 프로파일과 프로퍼티를 다룰 수 있다. - 프로파일 설정을 통해서 빈들을 묶음으로 다룬다.

--- src/main/java/com/example/inflearn_spring_core_demo/book/BookStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.inflearn_spring_core_demo.book;
-
-public enum BookStatus {
-    DRAFT, PUBLISHED
-}
--- src/main/java/com/example/inflearn_spring_core_demo/book/YourBookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.inflearn_spring_core_demo.book;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-// @Primary
-public class YourBookRepository implements BookRepository {
-}
